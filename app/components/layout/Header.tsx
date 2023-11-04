53dae85fb1640998e910566691c402516a7d8286
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2023-11-04 12:03:42 +0000

    feat: modal ready for notification form

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,8 +1,13 @@
 import { FC } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { BellIcon } from "@heroicons/react/24/outline";
 
 const Header: FC = () => {
+  const notificationModal = () => {
+    console.log('notification')
+  }
+
   return (
     <header className="py-10 mx-auto max-w-8xl">
       <nav className="relative z-50 flex justify-between mx-4">
@@ -10,7 +15,13 @@ const Header: FC = () => {
           <Image src="/logo.svg" alt="header-logo" width="40" height="40" />
           <h1 className="font-bold">RaccBook</h1>
         </Link>
-        <w3m-button />
+        <div className="flex items-center gap-4">
+          <div className="indicator animate-bounce cursor-pointer" onClick={notificationModal}>
+            <span className="indicator-item badge badge-sm badge-error"></span>
+            <BellIcon className="h-6 w-6 " />
+          </div>
+          <w3m-button balance="hide" />
+        </div>
       </nav>
     </header>
   );
